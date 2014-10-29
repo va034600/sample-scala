a78cfa7025a8d10e330fcc22205ae0d0a5044d7d
va034600 <va034600@gmail.com>
2014-10-29 16:41:18 +0900

    bootstrap導入

--- sample-play2.3-base/app/assets/javascripts/main.js
+++ sample-play2.3-base/app/assets/javascripts/main.js
@@ -1,16 +1,20 @@
 require.config({
 	baseUrl: "/assets/javascripts",
 	shim: {
-		'jquery': {
+		jquery: {
 			exports: [ '$' ]
+		},
+		bootstrap : {
+			deps : [ 'jquery' ]
 		}
 	},
 	paths : {
-		"jquery" : "/webjars/jquery/2.1.1/jquery.min"
+		jquery : "/webjars/jquery/2.1.1/jquery.min",
+		bootstrap : '/webjars/bootstrap/3.2.0/js/bootstrap.min'
 	}
 });
 
-require(["helper/Abc", "jquery"],function(Abc, $) {
+require(["helper/Abc", "jquery", "bootstrap"],function(Abc, $) {
 	var abc = new Abc();
 	alert(abc.sum(4, 5));
 	alert($(".test-button").text());
