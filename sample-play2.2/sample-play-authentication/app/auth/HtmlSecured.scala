dafbc6078fc7b7d2e70b416b65a40b131ac46b90
va034600 <va034600@gmail.com>
2014-11-29 21:46:08 +0900

    リファクタリング

--- sample-play2.2/sample-play-authentication/app/auth/HtmlSecured.scala
+++ sample-play2.2/sample-play-authentication/app/auth/HtmlSecured.scala
@@ -1,14 +1,15 @@
-package controllers
+package auth
 
 import play.api._
 import play.api.mvc._
 import play.api.data._
+import controllers.routes
 
-trait Secured {
+trait HtmlSecured {
 
   def username(request: RequestHeader) = request.session.get(Security.username)
 
-  def onUnauthorized(request: RequestHeader) = Results.Redirect(routes.Application.login)
+  def onUnauthorized(request: RequestHeader) = Results.Redirect(controllers.routes.Application.login)
 
   def withAuth(f: => String => Request[AnyContent] => Result) = {
     Security.Authenticated(username, onUnauthorized) { user =>
--- sample-play2.2/sample-play-authentication/app/controllers/BaseController.scala
+++ sample-play2.2/sample-play-authentication/app/controllers/BaseController.scala
@@ -4,13 +4,14 @@ import play.api._
 import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
+import auth.HtmlSecured
 
 /**
  * 参考
  * Authorization
  * http://www.playframework.com/documentation/2.0.1/ScalaSecurity
  */
-class BaseController extends Controller with Secured {
+class BaseController extends Controller with HtmlSecured {
   val loginForm = Form(
     tuple(
       "email" -> text,
