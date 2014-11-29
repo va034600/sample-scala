39f3f4f33044ca77f74c51e39d23e8f91aaa25be
va034600 <va034600@gmail.com>
2014-11-29 21:35:07 +0900

    json

---
+++ sample-play2.2/sample-play-authentication/app/controllers/JsonController.scala
@@ -0,0 +1,20 @@
+package controllers
+
+import scala.math.BigDecimal.int2bigDecimal
+
+import play.api.libs.json.JsBoolean
+import play.api.libs.json.JsNumber
+import play.api.libs.json.JsObject
+import play.api.libs.json.JsString
+import play.api.libs.json.Json
+import play.api.mvc.Action
+import play.api.mvc.Controller
+
+object JsonController extends BaseController {
+  def json1 = withAuth { username =>
+    implicit request =>
+    val map = Map("aaa" -> "bbb")
+    val json = Json.toJson(map)
+    Ok(json)
+  }
+}
