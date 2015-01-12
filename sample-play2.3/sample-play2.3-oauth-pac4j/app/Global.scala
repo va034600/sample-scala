64a3e5c8199c0d71ab83b830b89ae8a4d5382b3f
va034600 <va034600@gmail.com>
2015-01-12 19:00:34 +0900

    initial commit for google oauth by pac4j

--- sample-play2.3/sample-play2.3-oauth-pac4j/app/Global.scala
+++ sample-play2.3/sample-play2.3-oauth-pac4j/app/Global.scala
@@ -1,11 +1,10 @@
 import java.util.ResourceBundle
-
 import org.pac4j.core.client.Clients
 import org.pac4j.oauth.client.TwitterClient
 import org.pac4j.play.Config
-
 import play.api.Application
 import play.api.GlobalSettings
+import org.pac4j.oauth.client.Google2Client
 
 object Global extends GlobalSettings {
   override def onStart(app: Application) {
@@ -15,7 +14,12 @@ object Global extends GlobalSettings {
       resourceBundle.getString("twitter_api_key"), 
       resourceBundle.getString("twitter_api_secret"))
 
-    val clients = new Clients("http://127.0.0.1:9000/callback", twitterClient)
+    val google2Client = new Google2Client(
+      resourceBundle.getString("google_api_key"), 
+      resourceBundle.getString("google_api_secret"))
+    google2Client.setScope(Google2Client.Google2Scope.EMAIL)
+
+    val clients = new Clients("http://127.0.0.1:9000/callback", twitterClient, google2Client)
     Config.setClients(clients)
   }
 }
--- sample-play2.3/sample-play2.3-oauth-pac4j/app/controllers/Application.scala
+++ sample-play2.3/sample-play2.3-oauth-pac4j/app/controllers/Application.scala
@@ -14,10 +14,17 @@ object Application extends ScalaController {
   def index = Action { request =>
     val newSession = getOrCreateSessionId(request)
     val urlTwitter = getRedirectAction(request, newSession, "TwitterClient", "/twitter/index.html").getLocation()
-    Ok(views.html.index(urlTwitter)).withSession(newSession)
+    val urlGoogle = getRedirectAction(request, newSession, "Google2Client", "/google/index.html").getLocation()
+    Ok(views.html.index(urlTwitter, urlGoogle)).withSession(newSession)
   }
 
   def twitterIndex = RequiresAuthentication("TwitterClient") { profile =>
+    Action { request =>
+      Ok(views.html.protectedIndex(profile))
+    }
+  }
+
+  def googleIndex = RequiresAuthentication("Google2Client") { profile =>
     Action { request =>
       Ok(views.html.protectedIndex(profile))
     }
